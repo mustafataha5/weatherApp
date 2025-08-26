94639cda94be76e2c51c0c19e329d785efc9c9ee
mustafataha5 <mustafa.taha.mu95@gmail.com>
2025-08-26 15:06:25 +0300

    update

--- server/server.js
+++ server/server.js
@@ -43,9 +43,9 @@ if (process.env.NODE_ENV === "production") {
   const clientBuildPath = path.join(__dirname, "../client/dist");
   app.use(express.static(clientBuildPath));
 
-  app.get("*", (req, res) => {
-    res.sendFile(path.join(clientBuildPath, "index.html"));
-  });
+//   app.get("*", (req, res) => {
+//     res.sendFile(path.join(clientBuildPath, "index.html"));
+//   });
 }
 
 const PORT = process.env.PORT || 5000;
