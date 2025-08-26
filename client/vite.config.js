35ba047fd481c14317e5f26d9af1145ef37d23f5
mustafataha5 <mustafa.taha.mu95@gmail.com>
2025-08-26 12:46:02 +0300

    client

--- client/vite.config.js
+++ client/vite.config.js
@@ -4,7 +4,11 @@ import react from '@vitejs/plugin-react-swc'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-    server: {
-    port: 3000, // change port here
+  server: {
+    port: 3000,       // only used locally
+    strictPort: false,
+  },
+  build: {
+    outDir: 'build',  // ensures output goes to /client/build
   },
 })
